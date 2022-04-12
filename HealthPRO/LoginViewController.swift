3dc625fd45a726422e0c43ca5f03574f3a85fe89
Pranav Rajiv <pranav.rajiv@northwestern.edu>
2022-04-11 22:11:39 -0500

    Added faceid authentication

--- HealthPRO/LoginViewController.swift
+++ HealthPRO/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
@@ -17,11 +18,42 @@ class LoginViewController: UIViewController {
     }
 
     @objc private func loginProfile() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            if let controller = storyboard.instantiateViewController(withIdentifier: "tabBarVC") as? UITabBarController {
-                controller.modalPresentationStyle = .fullScreen
-                self.present(controller, animated: true, completion: nil)
+        
+        let context = LAContext()
+            var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Biometric needed for login"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        if let controller = storyboard.instantiateViewController(withIdentifier: "tabBarVC") as? UITabBarController {
+                            controller.modalPresentationStyle = .fullScreen
+                            self?.present(controller, animated: true, completion: nil)
+                        }
+                    } else {
+                        // error
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            // no biometric
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
         }
+
+        
+        
+        
+        
     }
 }
 
