0577822323f55e7fccaa8bc79fc63abb9847f0db
Alex Mamach <garthand@github.com>
2022-04-16 17:31:27 -0500

    Reworked BMR calculator

--- HealthPRO/Tabs/DietViewController.swift
+++ HealthPRO/Tabs/DietViewController.swift
@@ -8,62 +8,66 @@
 import UIKit
 
 class DietViewController: UIViewController {
-
+    var weight: Double!
+    var age: Double!
+    var height: Double!
+    var weight_units: String!
+    var height_units: String!
+    var sex: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var weight = 130.00
-        let age = 30.00
-        var height = 69.00
-        let weight_units = "lbs"
-        let height_units = "in"
-        let sex = "Male"
-        height = standardize_height_units(height: height, height_units: height_units)
-        weight = standardize_weight_units(weight: weight, weight_units: weight_units)
-        let bmr = calculate_bmr(weight: weight, height: height, age:age, sex:sex)
+        // TODO these variables should be pulled from the UI on the first
+        // run, or the database on subsequent runs
+        self.weight = 140
+        self.age = 29
+        self.height = 69
+        self.weight_units = "lbs"
+        self.height_units = "in"
+        self.sex = "Male"
+        standardize_height_units()
+        standardize_weight_units()
+        let bmr = calculate_bmr()
         print(bmr)
     }
     
-    func standardize_weight_units(weight: Double, weight_units: String) -> Double {
+    func standardize_weight_units() {
         // Convert all weights to kg for further processing
-        var new_weight: Double
-        if weight_units == "kg" {
-                return weight
+        if self.weight_units == "kg" {
+                return
         // Convert lbs to kg, rounding up as necessary
-        } else if weight_units == "lbs" {
-            new_weight = round(weight * 0.453592)
+        } else if self.weight_units == "lbs" {
+            self.weight = round(self.weight * 0.453592)
         // Convert stone to kg, rounding up as necessary
         } else {
-            new_weight = round(weight * 6.35029)
+            self.weight = round(self.weight * 6.35029)
         }
-        return new_weight
     }
     
-    func standardize_height_units(height: Double, height_units: String) -> Double {
+    func standardize_height_units() {
         // Ensures that all heights are converted to cm for further processing
-        var new_height: Double
         // If units are already in centimeters no conversion needed
-        if height_units == "cm" {
-            return height
+        if self.height_units == "cm" {
+            return
         // Convert inches to centimeters, rounding up as necessary
         } else {
-        new_height = round(height * 2.54)
+            self.height = round(height * 2.54)
         }
-        return new_height
     }
     
-    func calculate_bmr(weight: Double, height: Double, age: Double, sex: String) -> Double {
+    func calculate_bmr() -> Int {
         // Calculates basal metabolic rate using the Mifflin St Jeor equation
-        let weight_factor = 10.0 * weight
-        let height_factor = 6.25 * height
-        let age_factor = 5.0 * age
+        let weight_factor = 10.0 * self.weight
+        let height_factor = 6.25 * self.height
+        let age_factor = 5.0 * self.age
         let sex_factor: Double
         if sex == "Female" {
             sex_factor = -161.0
         } else {
             sex_factor = 5.0
         }
-        let bmr = weight_factor + height_factor - age_factor + sex_factor
+        let bmr = Int(weight_factor + height_factor - age_factor + sex_factor)
         return bmr
     }
 
