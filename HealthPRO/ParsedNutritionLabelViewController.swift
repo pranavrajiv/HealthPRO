a82ee00db485dfb9cb26380f2019af6ec5756b62
Pranav Rajiv <pranav.rajiv@northwestern.edu>
2022-04-17 00:03:17 -0500

    parsed data now gets populated in the textfeilds

--- HealthPRO/ParsedNutritionLabelViewController.swift
+++ HealthPRO/ParsedNutritionLabelViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 class ParsedNutritionLabelViewController: UIViewController {
 
     @IBOutlet weak var caloriesVal: UITextField!
-    @IBOutlet weak var totalFatVal: UIStackView!
-    @IBOutlet weak var cholesterolVal: UIStackView!
-    @IBOutlet weak var sodiumVal: UIStackView!
-    @IBOutlet weak var totalCarbohydratesVal: UIStackView!
-    @IBOutlet weak var dietaryFibersVal: UIStackView!
-    @IBOutlet weak var totalSugarsVal: UIStackView!
-    @IBOutlet weak var proteinVal: UIStackView!
-    @IBOutlet weak var calciumVal: UIStackView!
-    @IBOutlet weak var ironVal: UIStackView!
+    @IBOutlet weak var totalFatVal: UITextField!
+    @IBOutlet weak var cholesterolVal: UITextField!
+    @IBOutlet weak var sodiumVal: UITextField!
+    @IBOutlet weak var totalCarbohydratesVal: UITextField!
+    @IBOutlet weak var dietaryFibersVal: UITextField!
+    @IBOutlet weak var totalSugarsVal: UITextField!
+    @IBOutlet weak var proteinVal: UITextField!
+    @IBOutlet weak var calciumVal: UITextField!
+    @IBOutlet weak var ironVal: UITextField!
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     var ocrText: String = ""
+    var macro:[String:String]=["Calories":"","Total Fat":"","Total Carbs":"","Cholesterol":"","Sodium":"","Dietary Fiber":"","Sugars":"","Protein":"","Calcium":"","Iron":""]
 
     convenience init( ocrText: String ) {
         self.init()
@@ -46,9 +47,73 @@ class ParsedNutritionLabelViewController: UIViewController {
     }
 
     @objc private func processOcrText() {
+        let lines = self.ocrText.split(whereSeparator: \.isNewline)
+        
+        for (macroKey, _) in macro {
+            for (index, line) in lines.enumerated() {
+                print("\(index + 1). \(line)")
+                if  (macro[macroKey] == "") {
+                    if let macroIndex = line.index(of: macroKey) {
+                        if let value = self.getMacroValue(macroNutrientLine: String(lines[index][macroIndex...]), endingWith: "mg") {
+                            macro[macroKey] = value
+                        } else if let value = self.getMacroValue(macroNutrientLine: String(lines[index][macroIndex...]), endingWith: "g") {
+                            macro[macroKey] = value
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+        self.caloriesVal.text = macro["Calories"]
+        self.totalFatVal.text = macro["Total Fat"]
+        self.cholesterolVal.text = macro["Cholesterol"]
+        self.sodiumVal.text = macro["Sodium"]
+        self.totalCarbohydratesVal.text = macro["Total Carbs"]
+        self.dietaryFibersVal.text = macro["Dietary Fiber"]
+        self.totalSugarsVal.text = macro["Sugars"]
+        self.proteinVal.text = macro["Protein"]
+        self.calciumVal.text = macro["Calcium"]
+        self.ironVal.text = macro["Iron"]
         
     }
     
-    
-    
+    @objc private func getMacroValue(macroNutrientLine:String, endingWith:String)->String? {
+        
+        let trimmedString = macroNutrientLine.replacingOccurrences(of: " ", with: "")
+
+        let range = NSRange(location: 0, length: trimmedString.utf16.count)
+        let regex = try! NSRegularExpression(pattern: "[0-9]+"+endingWith)
+
+        let results = regex.matches(in: trimmedString,range: range)
+        
+        if let resultsString = (results.map {String(trimmedString[Range($0.range, in: trimmedString)!])}).first {
+            return String(resultsString.dropLast(endingWith.count)+" "+endingWith)
+        }
+        return nil
+    }
+}
+
+extension StringProtocol {
+    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.lowerBound
+    }
+    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.upperBound
+    }
+    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+        ranges(of: string, options: options).map(\.lowerBound)
+    }
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+            let range = self[startIndex...]
+                .range(of: string, options: options) {
+                result.append(range)
+                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
 }
