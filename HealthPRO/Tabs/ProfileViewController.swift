bcf8b74e824bd7d844ee6d8f47fc6d5240902f4b
Pranav Rajiv <pranav.rajiv@northwestern.edu>
2022-05-06 01:39:15 -0500

    minor change with ui

--- HealthPRO/Tabs/ProfileViewController.swift
+++ HealthPRO/Tabs/ProfileViewController.swift
@@ -19,5 +19,10 @@ class ProfileViewController: UIViewController {
         let viewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController as! LoginViewController
         viewController.cleanup()
     }
+    //Dismiss the keyboard when touched outside the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
 
 }
