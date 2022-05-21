030512007b1f31f77d3e17c63b948f7ad7546ee3
Pranav Rajiv <pranav.rajiv@northwestern.edu>
2022-05-20 22:01:36 -0500

    Suggestions UI complete

--- HealthPRO/SuggestionViewController.swift
+++ HealthPRO/SuggestionViewController.swift
@@ -13,23 +13,25 @@ class SuggestionsViewController: UIViewController{
     //@IBOutlet weak var historyButton: UIButton!
     var weatherUpdateTimer:Timer!
 
-    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var activitySuggestion: UILabel!
+    @IBOutlet weak var foodSuggestion: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.cancelButton.addTarget(self, action: #selector(cancelButtonTouchUp), for: .touchUpInside)
     
-        let currentWeatherHere:String = self.getTheWeather()
+        
+        //let currentWeatherHere:String = self.getTheWeather()
         var outdoorOkay = true;
         
-        if (currentWeatherHere.contains("SNOW") || currentWeatherHere.contains("RAIN")) {
-            outdoorOkay = false;
-        }
+//        if (currentWeatherHere.contains("SNOW") || currentWeatherHere.contains("RAIN")) {
+//            outdoorOkay = false;
+//        }
         
         //TODO: load in user's preference, load in suggestions and filter based on user preference and weather flag
         //TODO: present suggestion results per Storyboard format
         
-        if let _ = self.weatherUpdateTimer {
-            self.weatherUpdateTimer.invalidate()
-        }
+       
         
         //Uncomment this line once the close button is implemented for this VC otherwise there is a chance for a memory leak due to the timers not getting invalidated
         //self.weatherUpdateTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(getTheWeather), userInfo: nil, repeats: true)
@@ -37,7 +39,6 @@ class SuggestionsViewController: UIViewController{
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.weatherUpdateTimer.invalidate()
     }
     
     override func viewDidLoad() {
@@ -45,13 +46,17 @@ class SuggestionsViewController: UIViewController{
     
     }
     
-    @objc func getTheWeather() -> String {
-        let viewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController as! LoginViewController
-        
-        if let currentWeatherHere  = viewController.weatherInfoNow.currentWeather {
-            return currentWeatherHere.weather[0].description.capitalized
-        }
-        
-        return ""
+    @objc private func cancelButtonTouchUp() {
+        self.dismiss(animated: true)
     }
+    
+//    @objc func getTheWeather() -> String {
+//        let viewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController as! LoginViewController
+//
+//        if let currentWeatherHere  = viewController.weatherInfoNow.currentWeather {
+//            return currentWeatherHere.weather[0].description.capitalized
+//        }
+//
+//        return ""
+//    }
 }
